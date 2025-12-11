1ca68dfe07e30e5df2a830033a18855033876659
isidrohdez067177-gif <isidrohdez067177@gmail.com>
2025-12-11 15:02:37 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('sgpt-cache').then(c=>c.addAll(['./','manifest.json','index.html'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
